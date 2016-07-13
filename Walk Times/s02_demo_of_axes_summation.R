77a459c315fe4109eb1c507762b7370ac4b2ba89
Matin Kheirkhahan <matinkheirkhahan@ufl.edu>
2016-07-13 13:11:10 -0400

    Fixed the address of shared drive in demo script.

--- Walk Times/s02_demo_of_axes_summation.R
+++ Walk Times/s02_demo_of_axes_summation.R
@@ -13,7 +13,7 @@ library(XLConnect) # Loading the library.
 source("f02_axes_summation.R")
 
 xlsx_file_start_and_end <- "update_walktime_found0518.xlsx"
-dataFiles_folder <- "~/../../Volumes/SHARE/ARRC/Active_Studies/ANALYSIS_ONGOING/LIFE Main data/LIFE accelerometry - second data - 10_26_15/"
+dataFiles_folder <- "V:/ARRC/Active_Studies/ANALYSIS_ONGOING/LIFE Main data/LIFE accelerometry - second data - 10_26_15/"
 visit_seq <- 6
 outputFile_csv <- "axes_sum.csv"
 axes.sum.df <- generate.axes.sum(xlsx_file_start_and_end, dataFiles_folder, visit_seq, outputFile_csv)
